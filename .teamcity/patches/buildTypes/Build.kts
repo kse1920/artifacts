8577d3b26765df38ed04c65afeef219db319a330
kse1920 <kse1920@TeamCity>
2019-06-05 19:43:38 +0300

    TeamCity change in 'DSL152' project: general settings of 'Build' build configuration were updated

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -1,6 +1,9 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2018_2.ideaInspections
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
 
 /*
@@ -12,5 +15,27 @@ changeBuildType(RelativeId("Build")) {
     check(description == "") {
         "Unexpected description: '$description'"
     }
-    description = "kjhds"
+    description = "test"
+
+    expectSteps {
+        maven {
+            goals = "clean test"
+            pomLocation = "artifact/pom.xml"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+        script {
+            scriptContent = """echo "test""""
+        }
+        ideaInspections {
+            pathToProject = "artifact/pom.xml"
+            jvmArgs = "-Xmx512m -XX:ReservedCodeCacheSize=240m"
+            targetJdkHome = "%env.JDK_18%"
+        }
+    }
+    steps {
+        check(stepsOrder == arrayListOf("RUNNER_1", "RUNNER_2", "RUNNER_3")) {
+            "Unexpected build steps order: $stepsOrder"
+        }
+        stepsOrder = arrayListOf<String>()
+    }
 }
